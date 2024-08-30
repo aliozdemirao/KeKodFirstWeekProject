4537fad420b52027c26c716ead09bf76f01bfc94
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2024-08-30 14:35:28 +0300

    Setup NavHostFragment and provide BottomNavigationView access
    - Setup NavHostFragment and link it with BottomNavigationView
    - Add method getBottomNavigationView to provide access to BottomNavigationView for fragments
    - Override onSupportNavigateUp to handle navigation up events

--- app/src/main/java/com/aliozdemir/kekodfirstweekproject/MainActivity.kt
+++ app/src/main/java/com/aliozdemir/kekodfirstweekproject/MainActivity.kt
@@ -2,10 +2,26 @@ package com.aliozdemir.kekodfirstweekproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.aliozdemir.kekodfirstweekproject.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
     }
+
+    fun getBottomNavigationView(): BottomNavigationView = binding.bottomNavigationView
+
+    override fun onSupportNavigateUp(): Boolean = navHostFragment.navController.navigateUp() || super.onSupportNavigateUp()
 }
